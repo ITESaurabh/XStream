5ea8ec237d07e68b9a745bf8feb841d5878c7b39
ITESaurabh <itesaurabh@gmail.com>
2020-01-09 01:36:06 +0530

    update

---
+++ application/views/devs.php
@@ -0,0 +1,74 @@
+
+<div class="container">
+
+<div class="card-deck pb-4">
+  <div class="card p-4">
+
+    <div class="card-body">
+    <h3 class="card-title">Saurabh Tanwar</h3>
+        <p class="card-title"><b>Full Stack Developer</b></p>
+        <p class="card-text"><a href="http://ITESaurabh.gq">ITESaurabh.gq</a></p>
+        <p class="card-text">itesaurabh@yahoo.com</p>
+     
+    </div>
+  </div>
+  <div class="card p-4">
+    
+    <div class="card-body">
+    <h3 class="card-title">Lalit Patidar</h3>
+        <p class="card-title"><b>Back-end Developer</b></p>
+       
+        <p class="card-text">lytt10017@gmail.com </p>
+     
+    </div>
+  </div>
+  <div class="card p-4">
+   
+    <div class="card-body">
+    <h3 class="card-title">Ravi Vishwakarma</h3>
+        <p class="card-title"><b>Database Developer</b></p>
+       
+        <p class="card-text">Email:</p>
+     
+    </div>
+  </div>
+</div>
+
+
+<div class="card-deck pb-4">
+  <div class="card p-4">
+
+    <div class="card-body">
+    <h3 class="card-title">Kajal Singh</h3>
+        <p class="card-title"><b>Front-End Developer</b></p>
+       
+        <p class="card-text">kajalsingh1616@gmail.com</p>
+     
+    </div>
+  </div>
+  <div class="card p-4">
+    
+    <div class="card-body">
+    <h3 class="card-title">Rishika Jain</h3>
+        <p class="card-title"><b>Back-End Developer</b></p>
+       
+        <p class="card-text">Email:</p>
+     
+    </div>
+  </div>
+  <div class="card p-4">
+   
+    <div class="card-body">
+    <h3 class="card-title">Praveen Yadav</h3>
+        <p class="card-title"><b>Research & Development</b></p>
+       
+        <p class="card-text">praveenyadavtak@gmail.com</p>
+     
+    </div>
+  </div>
+</div>
+
+
+
+
+</div>
