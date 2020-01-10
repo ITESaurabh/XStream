c30617b9e061343821a05ed7b850b6197c9e6fd3
ITESaurabh <itesaurabh@gmail.com>
2020-01-10 11:25:13 +0530

    Co-authored-by: lytt10017@gmail.com <lytt10017@gmail.com> Co-authored-by: rishikajain290@gmail.com <rishikajain290@gmail.com>

--- application/views/navbar/nav.php
+++ application/views/navbar/nav.php
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top static-top mb-5 shadow">
 <?php }?>
     <div class="container">
-      <a class="navbar-brand" href="#">The Art Archive</a>
+      <a class="navbar-brand" href="#">X-Stream</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
